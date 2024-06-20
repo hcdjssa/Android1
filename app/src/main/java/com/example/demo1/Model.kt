7c52f348f8f4a03212a5f5bb33fdc0bfb989e5ce
hcdjssa <128838445+hcdjssa@users.noreply.github.com>
2024-06-20 16:47:54 +0530

    java to kotlin

---
+++ app/src/main/java/com/example/demo1/Model.kt
@@ -0,0 +1,7 @@
+package com.example.demo1
+
+class Model {
+    var s:Student=Student("shiva",21,"abc")
+
+
+}
---
+++ app/src/main/java/com/example/demo1/Student.kt
@@ -0,0 +1,4 @@
+package com.example.demo1
+
+class Student(var name: String, var age: Int, var postalCode: String)
+
